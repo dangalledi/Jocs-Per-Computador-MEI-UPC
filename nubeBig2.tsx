b9e70c1650b951f770edeb08c774456b764da21b
AlexandraOliv14 <54546485+AlexandraOliv14@users.noreply.github.com>
2024-03-01 20:29:08 +0100

    clean imagenes

--- nubeBig2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="nubeBig2" tilewidth="32" tileheight="32" tilecount="8" columns="4">
- <image source="nubeBig2.png" width="128" height="64"/>
-</tileset>
